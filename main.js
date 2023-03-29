2457b11c1bd3a2ebde13b1e5b942021e3c33f5e3
Tobias Duarte <tobiiduarte95@gmail.com>
2023-03-29 17:49:42 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,178 @@
+// nombre = prompt ("ingrese su nombre");
+// edad = prompt ("ingrese su edad");
+// pais = prompt ("ingrese su País");
+
+// console.log (`Hola! me llamo ${nombre}, tengo ${edad} años y soy de ${pais}`)
+// console.log("Hola mundo!");
+
+// var a = "Estoy en global";
+// {
+//     var b = "Estoy en bloque";
+// }
+// {
+//     let c = "Estoy en bloque c";
+//     console.log(c);
+
+// }
+// {
+//     const d = "Estopy en bloque D"
+// }
+
+//     console.log(a);
+//     console.log(b);
+
+// let numero = 5;
+// numero *= 4;
+// numero++;
+// console.log(numero);
+// console.log(18==="12");
+// console.log(14===14);
+// console.log(14=="14");
+// console.log(13==13);
+// console.log(13=="13");
+// console.log(13==="13");
+// console.log(13!="13");
+// console.log(13!=="13");
+// console.log(13!==13);
+// console.log(13!==12);
+// console.log(13>12);
+// console.log(15<14);
+// console.log(16<16);
+// console.log(16<19);
+// console.log(10<=11);
+// console.log(10<=10);
+
+// console logg endd
+
+// var mostrarMensaje = false;
+
+// if (mostrarMensaje) {
+//     alert("Hola mundo!")
+// } else {
+//     alert("Chau mundo")
+// }
+
+// var edad = prompt ("Decime tu edad, pls")
+// if (edad >= 18) {
+//     console.log ("Sos mayor capo, felicidades");
+// }   else {
+//     alert("sori, sos menor. chau!")
+// }
+
+// var heladoPreferido = prompt("Decime tu gusto de helado preferido, por favor... (limon, chocolate, frutilla, crema")
+// if (heladoPreferido === "chocolate") {
+//     console.log("Te gusta el chocolate!");
+// } else if (heladoPreferido === "limon") {
+//     console.log("Te gusta el limon!!");
+// } else if (heladoPreferido === "crema") {
+//     console.log("Te gusta de crema!!!");
+// } else if (heladoPreferido === "frutilla") {
+//     console.log("hijo de puta no te puedde gustar la frutilla, asqueroso");
+// }
+
+// var heladoPreferido = prompt("Decime tu gusto de helado preferido, por favor... (limon, chocolate, frutilla, crema")
+// switch (heladoPreferido) {
+//     case "crema":
+//         console.log("Te gusta de crema!!!");
+//         break;
+//     case "frutilla":{
+//         console.log("hijo de puta no te puedde gustar la frutilla, asqueroso")
+//         break
+//     }
+//     case "chocolate": {
+//         console.log("Te gusta el chocolate!");
+//         break
+//     }
+//     case "limon": {
+//         console.log("Te gusta el limon!!");
+//     }
+//     default: "no me gusta el helado"
+//         console.log("perfecto, matate :)");
+//         break;
+// }
+
+// let nombre = prompt ("Tu nombre?");
+// let edad = 20;
+// if (nombre === "Tobias") {
+//     console.log("Perfecto! Eres Tobias :)");
+// } else {
+//     randomName = prompt ("Oye! Quien eres?");
+//     alert (`oh! Hola ${randomName} ¿how are you doing?`);
+// }
+// let nombre = prompt("su nombre");
+// let edad = prompt("Su edad");
+// console.log(`Hola! Me llamo ${nombre} y tengo ${edad} años. Gracias por invitarme!`);
+
+// let numeroA = prompt ("ingrese un numero")
+// let numeroB = prompt ("ingrese el numero que quiera sumar")
+// let resultado = (numeroA + numeroB)
+//     console.log(resultado);
+
+// const nombre = 10;
+// const edad = 26;
+// console.log(nombre + edad);
+
+// let numero = 15;
+// numero = 25;
+// alert(numero)
+
+// let valor = prompt("Ingrese su valor");
+// let valorB = prompt("Ingrese su segundo valor")
+// alert(`El primer valor ingresado es: ${valor} y el segundo: ${valorB}`)
+
+// let numero = 18;
+// let numeroB = 20;
+// console.log(numero === numeroB);
+
+// let edad = prompt("Ingrese su edad");
+// if (edad >= 18) {
+//     console.log("Soy mayor de edad");
+// } else {
+//     console.log("Soy menor de edad");
+// }
+
+// let valor1 = 30;
+// let valor2 = 30;
+// if (valor1 > valor2) {
+//     console.log("valor1 es mayor que valor2");
+// } else if(valor1 < valor2) {
+//     console.log("valor1 es menor que valor2");
+// } else {
+//     console.log("ni uno ni otro");
+// }
+// let valor = prompt("ingrese un valor");
+// switch (valor) {
+//     case "alerta":
+//         alert("Soy una alerta")
+//         break;
+//     case "consola":
+//         console.log("soy un mensaje en la consoola");
+//         break;
+//     default:
+//         alert("me muestro en ambos lados")
+//         console.log("Me muestro en ambos lados");
+//         break;
+// }
+// let edad = 12;
+// switch (edad) {
+//     case edad < 20, edad < 13:
+//         console.log("Soy un nino");
+//         break;
+//     case edad < 20, edad >= 13:
+//         console.log("Soy un adolecente");
+//     default:
+//         break;
+// }
+
+let fruta = prompt("Ingrese una fruta");
+ switch (fruta) {
+    case "frutilla":
+        console.log(`Soy una ${fruta}`);
+        break;
+    case "palta":
+        console.log(`Soy una ${fruta}`);
+        break
+    default:
+        console.log("nno se que soy :(palta");
+        break;
+ }
